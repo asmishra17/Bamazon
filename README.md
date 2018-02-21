656cb6720eb3663c1c0c2bf291e99d27fcc63448
Aditi Mishra <asmishra@Aditis-MacBook-Air.local>
2018-02-20 22:52:53 -0600

    working on submission requirements

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Bamazon
-Week 12 Homework
+#Bamazon
+
+In this application, I created an **Amazon-like** storefront using *MySQL* and *JavaScript*. The app will take in orders from customers and deplete stock from the store's inventory.  
---
+++ bamazonSeeds.sql
@@ -0,0 +1,5 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Air Mattress", "Outdoors", 40.00, 100), ("Soylent", "Food", 29.99, 90), ("Watch", "Jewelry", 150.00, 80), ("Lotion", "Personal Care", 9.99, 70), ("Book", "Books", 15.00, 60), ("Bamazon Echo", "Electronics", 100.00, 50), ("Dress", "Clothing", 19.99, 40), ("Granola Bars", "Food", 9.50, 30), ("Gloves", "Clothing", 5.00, 20), ("Fitbit", "Electronics", 100.00, 10);
+
